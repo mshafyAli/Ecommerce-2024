7c2fa72fd09b195035947d767f08fbd24be493f7
shafyali <shafyali433@gmail.com>
2025-01-01 03:15:45 +0500

    craete static products forms

--- client/src/pages/admin-view/products.jsx
+++ client/src/pages/admin-view/products.jsx
@@ -1,8 +1,58 @@
+import CommonForm from "@/components/common/form";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { addProductFormElements } from "@/config";
+import { useState } from "react";
+
+const initialFormData = {
+  image : null,
+  title: "",
+  description: "",
+  category: "",
+  brand: "",
+  price: "",
+  salePrice: "",
+  stock: "",
+  totalStock: "",
+};
+
 
 const AdminProducts = () => {
-  return (
-    <div>AdminProducts</div>
-  )
-}
+  const [openCreateProductsDialog, setOpenCreateProductsDialog] =
+  useState(false);
+  const [formData, setFormData] = useState(initialFormData);
 
-export default AdminProducts
+  const onSubmit = () => {
+
+  }
+
+
+  return (
+    <>
+      <div className="mb-5 w-full flex justify-end">
+        <Button onClick={()=>setOpenCreateProductsDialog(true)}>Add Product</Button>
+        <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4">
+          <Sheet open={openCreateProductsDialog} onOpenChange={()=>setOpenCreateProductsDialog(false)}>
+            <SheetContent side="right" className="overflow-hidden">
+              <SheetHeader>
+                <SheetTitle>Add New Product</SheetTitle>
+              </SheetHeader>
+
+              <div className="py-6">
+                <CommonForm formData={formData} setFormData={setFormData} buttonText="Add"
+                onSubmit={onSubmit} formControls={addProductFormElements}/>
+              </div>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AdminProducts;
